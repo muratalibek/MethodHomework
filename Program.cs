4e8a4cc513fd98c7ff4f3120a0e3b3d0bd41c7c8
muratBee01 <muratalibek01@gmail.com>
2022-10-26 18:51:56 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MethodHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            string name = Console.ReadLine();
+
+            Show(name, num);
+        }
+        static string Show(string name, int num)
+        {
+            string fullText = "Hello, Dear " + name;
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine(fullText);
+            }
+            return fullText;
+        }
+    }
+}
